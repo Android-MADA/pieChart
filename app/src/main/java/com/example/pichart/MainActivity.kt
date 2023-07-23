692793c86f50b5bc4bc17f58b96d1445ce4a43b6
Beomsu <qjatn0521@kw.ac.kr>
2023-07-23 18:55:54 +0900

    파이차트 완성(그림자효과는 x)

--- app/src/main/java/com/example/pichart/MainActivity.kt
+++ app/src/main/java/com/example/pichart/MainActivity.kt
@@ -3,41 +3,74 @@ package com.example.pichart
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pichart.databinding.ActivityMainBinding
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var customCircleBarView: CustomCircleBarView       //프로그래스바
+    data class PieChartData(
+        val title: String,
+        val memo: String,
+        val startHour: Int,
+        val startMin: Int,
+        val endHour: Int,
+        val endMin: Int,
+        val colorCode: String,
+        val divisionNumber: Int
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding = ActivityMainBinding.inflate((layoutInflater))
         setContentView(binding.root)
 
+        customCircleBarView = binding.progressbar
+        // 원형 프로그레스 바 진행 상태 변경 (0부터 100까지)
+        customCircleBarView.setProgress(36)
+
+        //파이차트
         var chart = binding.chart
-        chart.setUsePercentValues(true)
-
+        val pieChartDataArray = arrayOf(        //임시 데이터
+            PieChartData("제목1", "메모1", 0,0,1,0, "#486DA3",0),      //제목, 메모, 시작 시각, 시작 분, 끝 시각, 끝 분, 색깔 코드, 구분 숫자
+            PieChartData("제목2", "메모2", 1,0,6,0, "#516773",1),
+            PieChartData("제목3", "메모3", 9,0,12,0, "#FDA4B4",2),
+            PieChartData("제목4", "메모4", 12,0,13,30, "#52B6C9",3),
+            PieChartData("제목5", "메모5", 13,30,14,30, "#516773",4),
+            PieChartData("제목6", "메모6", 14,30,16,30, "#52B6C9",5),
+            PieChartData("제목7", "메모7", 16,30,18,0, "#FCE79A",6),
+            PieChartData("제목8", "메모8", 20,0,22,0, "#486DA3",7),
+            PieChartData("제목9", "메모9", 22,0,24,0, "#FCE79A",8)
+        )
+        val marker_ = YourMarkerView(this, R.layout.custom_label_layout,pieChartDataArray)
         val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(20f, "apple"))
-        entries.add(PieEntry(20f, "Orange"))
-        entries.add(PieEntry(20f, "Mango"))
-        entries.add(PieEntry(20f, "RedOrange"))
-        entries.add(PieEntry(20f, "other"))
-
         val colorsItems = ArrayList<Int>()
-        for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.JOYFUL_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.COLORFUL_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.LIBERTY_COLORS) colorsItems.add(c)
-        for (c in ColorTemplate.PASTEL_COLORS) colorsItems.add(c)
-        colorsItems.add(ColorTemplate.getHoloBlue())
+
+        var tmp = 0     //시작 시간
+
+       for(data in pieChartDataArray) {
+           val start = data.startHour.toString().toInt() * 60 + data.startMin.toString().toInt()
+           val end = data.endHour.toString().toInt() * 60 + data.endMin.toString().toInt()
+           if(tmp==start) {      //이전 일정과 사이에 빈틈이 없을때
+               entries.add(PieEntry((end-start).toFloat(), data.divisionNumber.toString()))
+               colorsItems.add(Color.parseColor(data.colorCode.toString()))
+               tmp = end
+           } else {
+               val noScheduleDuration = start - tmp
+               entries.add(PieEntry(noScheduleDuration.toFloat(), "999"))      // 스케줄 없는 시간
+               colorsItems.add(Color.parseColor("#FFFFFF"))
+               entries.add(PieEntry((end-start).toFloat(), data.divisionNumber.toString()))
+               colorsItems.add(Color.parseColor(data.colorCode.toString()))
+               tmp = end
+           }
+       }
 
         // 왼쪽 아래 설명 제거
         val legend = chart.legend
@@ -47,20 +80,25 @@ class MainActivity : AppCompatActivity() {
         val pieDataSet = PieDataSet(entries, "")
         pieDataSet.apply {
             colors = colorsItems
-            valueTextColor = Color.BLACK
-            valueTextSize = 16f
             setDrawValues(false) // 비율 숫자 없애기
+
         }
 
         val pieData = PieData(pieDataSet)
+
+
+
         chart.apply {
             data = pieData
-            description.isEnabled = true
-            isRotationEnabled = false
-            isDrawHoleEnabled = false
-
-            setDrawEntryLabels(false) // 라벨 끄기
-            rotationAngle = 30f // 회전 각도
+            isRotationEnabled = false                               //드래그로 회전 x
+            isDrawHoleEnabled = false                               //중간 홀 그리기 x
+            setExtraOffsets(20f,20f,20f,20f)    //크기 조절
+            setUsePercentValues(false)
+            setEntryLabelColor(Color.BLACK)
+            marker = marker_
+            setDrawEntryLabels(false) //라벨 끄기
+            //rotationAngle = 30f // 회전 각도, 굳이 필요 없을듯
+            description.isEnabled = false   //라벨 끄기 (오른쪽아래 간단한 설명)
         }
         chart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
             override fun onValueSelected(e: Entry?, h: Highlight?) {
@@ -68,15 +106,19 @@ class MainActivity : AppCompatActivity() {
                     val pieEntry = e as PieEntry
                     val label = pieEntry.label
 
-                    // 라벨을 사용하여 원하는 동작을 수행합니다.
-                    Log.v("Selected Label",label)
+                    if (label == "999") {
+                        pieDataSet.selectionShift = 1f //하이라이트 크기
+                    } else {
+                        pieDataSet.selectionShift = 36f // 다른 라벨의 경우 선택 시 하이라이트 크기 설정
+                    }
                 }
             }
-
             override fun onNothingSelected() {
                 // 아무 것도 선택되지 않았을 때의 동작을 구현합니다.
             }
         })
+
+
 
 
     }
